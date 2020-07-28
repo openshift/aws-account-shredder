059444424deb27fb59786df9d36238e25323889c
agautam <amol_gautam25@yahoo.co.in>
2020-07-28 10:11:50 -0400

    app-sre integration 3

--- project.mk
+++ project.mk
@@ -2,7 +2,7 @@ OPERATOR_NAME?=$(shell sed -n 's/.*OperatorName .*"\([^"]*\)".*/\1/p' config/con
 OPERATOR_NAMESPACE?=$(shell sed -n 's/.*OperatorNamespace .*"\([^"]*\)".*/\1/p' config/config.go)
 
 IMAGE_REGISTRY?=quay.io
-IMAGE_REPOSITORY?=$(USER)
+IMAGE_REPOSITORY?=aws-account-shredder
 IMAGE_NAME?=$(OPERATOR_NAME)
 
 VERSION_MAJOR?=0
