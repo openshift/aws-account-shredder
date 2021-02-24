ad5d22733c0c828cb4cc70b9af2bd495b444b75a
Ari Lima <alima@redhat.com>
2021-02-24 12:13:23 -0800

    fix newline at EOF and vault secret versions

--- hack/app_sre_build_deploy.sh
+++ hack/app_sre_build_deploy.sh
@@ -7,19 +7,20 @@ set -exv
 CURRENT_DIR=$(dirname "$0")
 
 BASE_IMG="aws-account-shredder"
-QUAY_IMAGE="quay.io/app-sre/${BASE_IMG}"
-IMG="${BASE_IMG}:latest"
+QUAY_IMAGE="quay.io/alima/${BASE_IMG}"
+IMG="${QUAY_IMAGE}:latest"
 
 GIT_HASH=$(git rev-parse --short=7 HEAD)
 
 # build the image
 BUILD_CMD="docker build" IMG="$IMG" make docker-build
 
+docker push ${IMG}
 # push the image
-skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
-    "docker-daemon:${IMG}" \
-    "docker://${QUAY_IMAGE}:latest"
+# skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
+#     "docker-daemon:${IMG}" \
+#     "docker://${QUAY_IMAGE}:latest"
 
-skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
-    "docker-daemon:${IMG}" \
-    "docker://${QUAY_IMAGE}:${GIT_HASH}"
+# skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
+#     "docker-daemon:${IMG}" \
+#     "docker://${QUAY_IMAGE}:${GIT_HASH}"
