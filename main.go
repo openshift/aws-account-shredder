c0e82865dcf4d7bbf8ab09fc1e2c393d37da9ef1
Matthew Stratton <mstratto@redhat.com>
2022-03-21 14:06:34 -0400

    Revert "adding missing supported regions to shredder"
    This reverts commit 1ed1633df5135e977a45a7290b8bace946f575bf.

--- main.go
+++ main.go
@@ -29,9 +29,7 @@ const (
 )
 
 var (
-	// regions AAO is deployed to, should be kept up to date with https://github.com/openshift/aws-account-operator/blob/master/hack/olm-registry/olm-artifacts-template.yaml#L78
-	// (gov regions can be ignored for now)
-	supportedRegions = []string{"us-east-1", "us-east-2", "us-west-1", "us-west-2", "ca-central-1", "eu-north-1", "eu-central-1", "eu-west-1", "eu-west-2", "eu-west-3", "eu-south-1", "ap-northeast-1", "ap-northeast-2", "ap-northeast-3", "ap-east-1", "ap-south-1", "ap-southeast-1", "ap-southeast-2", "ap-southeast-3", "sa-east-1", "af-south-1", "me-south-1"}
+	supportedRegions = []string{"us-east-1", "us-east-2", "us-west-1", "us-west-2", "ca-central-1", "eu-central-1", "eu-west-1", "eu-west-2", "eu-west-3", "ap-northeast-1", "ap-northeast-2", "ap-south-1", "ap-southeast-1", "ap-southeast-2", "sa-east-1"}
 	log              = logf.Log.WithName("shredder_logger")
 )
 
